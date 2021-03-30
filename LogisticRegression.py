Fix Gradient Decent and Cost Function

LogisticRegression.py
@@ -7,28 +7,27 @@ class LogisticRegression:
         self.n_iterations = n_iterations
         self.learning_rate = learning_rate
         self.weights = None
-        self.bias = None
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
+    def train(self, x, y):
+        n_samples, n_features = x.shape
         self.weights = np.zeros(n_features)
-        self.bias = 0
+
+        cost_list = []
 
         for _ in range(self.n_iterations):
-            linear_model = np.dot(X, self.weights) + self.bias
+            linear_model = np.dot(x, self.weights)
             y_predicted = self._sigmoid(linear_model)
 
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
+            cost = -(1/n_features)*np.sum( y*np.log(y_predicted) + (1-y)*np.log(1-y_predicted))
+
+            # Gradient
+            dw = (1 / n_features) * np.dot(x.T, (y_predicted - y))
 
             self.weights -= self.learning_rate * dw
-            self.bias -= self.learning_rate * db
 
-    def predict(self, X):
-        linear_model = np.dot(X, self.weights) + self.bias
-        y_predicted = self._sigmoid(linear_model)
-        y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
-        return y_predicted_cls
+            cost_list.append(cost)
+
+        return cost_list
 
     @staticmethod
     def _sigmoid(x):